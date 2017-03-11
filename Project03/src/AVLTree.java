aceaef6a0c50b41981f3c78915ee72e17bb776bc
Yoga Lin <blueandhack@gmail.com>
2017-03-10 23:44:06 -0700

    Added Project03

---
+++ Project03/src/AVLTree.java
@@ -0,0 +1,235 @@
+/* class AVLTree
+ *
+ * Models a complete AVLTree.  Individual nodes are modeled by the AVLNode
+ * class.
+ *
+ * The constructor creates an empty tree (root=null).
+ *
+ * This class has many methods which perform basic operations.  Most of these
+ * (non-static) methods have (static) recursive helper methods, which take an
+ * AVLNode as the first paramter; these are static because it is valid for the
+ * parameter to be null.  Thus, the base case for each of these methods is an
+ * empty tree.  (See the method documentation below for details; there are a
+ * few exceptions to this rule.)
+ *
+ * Methods that perform modifications on the tree all use the x=change(x)
+ * style.  These include highly visible methods (such as insert and delete),
+ * and also utility methods (such as rotateRight).
+ */
+public class AVLTree {
+	// ALERT
+	//
+	// You *must not* add any fields other than this one to this class!
+	// This field is sufficient for everything that you need to do.
+	//
+	// Also, remember that you can't change the AVLNode class *at all* -
+	// and so you cannot add fields there, either!
+	//
+	// However, you can (and should!) add some static helper methods to
+	// this class.
+	private AVLNode root;
+
+	/*
+	 * default constructor
+	 *
+	 * Initializes this object to represent an empty tree.
+	 */
+	public AVLTree() {
+		// See my comment in the constructor for AVLNode. I like
+		// to explicitly set things to null, instead of simply
+		// using the default values.
+		// - Russ
+		root = null;
+	}
+
+	/*
+	 * debug(String)
+	 *
+	 * Opens a file with the given filename, and then fills that file with .dot
+	 * file source code, to generate a picture for this tree. This wrapper opens
+	 * (and closes) the file, and adds the necessary prefix/suffix text inside
+	 * it. However, the actual body of the file should be printed by a recursive
+	 * helper method.
+	 *
+	 * - Note from Russ: I have not yet defined the helper method, since I
+	 * assume that different students might use different Java classes for
+	 * printing. One option is to pass a FileWriter as a parameter; another is
+	 * to *return* a String.
+	 */
+	public void debug(String filename) {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * print_inOrder()
+	 *
+	 * Prints out an in-order traversal of the tree to stdout (followed by a
+	 * newline). The keys are separated by spaces, and the list (if non-empty)
+	 * has a trailing space. If the tree is empty, this prints out nothing
+	 * except for the newline. Uses a static recursive helper method.
+	 *
+	 * static print_inOrder(AVLNode)
+	 *
+	 * Helper method for the public method. Is static; takes an AVLNode
+	 * parameter, which might be null. If the parameter is null, it does
+	 * nothing. Works recursively.
+	 */
+	public void print_inOrder() {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	private void print_inOrder(AVLNode tree) {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * print_preOrder()
+	 *
+	 * See print_inOrder() above - works in exactly the same way, except
+	 * produces a pre-order traversal instead of an in-order traversal.
+	 */
+	public void print_preOrder() {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * search(int)
+	 *
+	 * Searches the tree for a given key. Returns the node where the tree
+	 * exists, or null if the key does not exist. May be implemented recursively
+	 * (with a helper method), or as a loop.
+	 */
+	public AVLNode search(int val) {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * insert(int)
+	 *
+	 * Inserts a new key into the tree; throws IllegalArgumentException if the
+	 * key is a duplicate.
+	 *
+	 * Uses the x=change(x) style to modify the tree. Uses a static, recursive
+	 * helper method to implement the modification. The helper method is
+	 * responsible for *all* of the changes associated with inserting a new
+	 * value, including all rebalances.
+	 *
+	 * static insert(AVLNode,int)
+	 *
+	 * Static, recursive helper method for insert(int). Takes an AVLNode
+	 * parameter, which might be null; if the parameter is null, then it creates
+	 * a new node and returns it.
+	 *
+	 * Always returns the root of the new tree; does all of the rebalancing work
+	 * associated with the insert.
+	 */
+	public void insert(int val) throws IllegalArgumentException {
+		root = insert(root, val);
+	}
+
+	private static AVLNode insert(AVLNode subtree, int val) throws IllegalArgumentException {
+		throw new RuntimeException("TODO: implemenet me");
+	}
+
+	/*
+	 * delete(int)
+	 *
+	 * Deletes a key from the tree; throws IllegalArgumentException if the key
+	 * does not exist.
+	 *
+	 * Uses the x=change(x) style to modify the tree. Uses a static, recursive
+	 * helper method to implement the modification. The helper method is
+	 * responsible for *all* of the changes associated with deleting the value,
+	 * including all rebalances.
+	 *
+	 * static delete(AVLNode,int)
+	 *
+	 * Static, recursive helper method for delete(int). Takes an AVLNode
+	 * parameter, which might be null; if the parameter is null, then we are
+	 * attempting to delete from an empty tree, and thus the delete fails. On
+	 * the other hand, this method can *return* null quite normally; this
+	 * happens when we delete the last node from the tree.
+	 *
+	 * Always returns the root of the new tree (which might be null); does all
+	 * of the rebalancing work associated with the delete. (But see note in the
+	 * spec about possibly skipping rebalance-on-delete.)
+	 */
+	public static void delete(int val) throws IllegalArgumentException {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * static rebalance(AVLNode)
+	 *
+	 * Performs a rebalance (if required) on the current node. This method
+	 * assumes that the parameter is non-null; if it is passed a null parameter,
+	 * it will hit NullPointerException. However, it is valid for either (or
+	 * both) of the children of this node to be null; to be clear, it is OK to
+	 * call this on a leaf node.
+	 *
+	 * This method assumes that the height is correct, in the current node and
+	 * all its descendants; thus, after an insert or delete, the *CALLER* must
+	 * update the height before calling this method.
+	 *
+	 * This method returns the root of the new tree. If no rotations were
+	 * required, then this is simply the parameter; if rotations were required,
+	 * then this returns the root after the rotation.
+	 *
+	 * This method is NON-RECURSIVE. It runs in O(1) time. As such, it only
+	 * checks for imbalances at THIS PARTICULAR NODE, never at any descendants.
+	 */
+	public static AVLNode rebalance(AVLNode node) {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * static getHeight(AVLNode)
+	 *
+	 * Helper method that returns the height of a subtree. The parameter may be
+	 * null.
+	 *
+	 * Since a single node has height=0, this should return -1 if passed a null
+	 * parameter.
+	 *
+	 * This method is NON-RECURSIVE. It runs in O(1) time. As such, it may only
+	 * check the height field in the current node; it does not look at any other
+	 * nodes.
+	 */
+	public static int getHeight(AVLNode subtree) {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * static rotateRight(AVLNode)
+	 *
+	 * Performs a right-rotation at the current node. This method assumes that
+	 * both the parameter, and its left child, are non-null; if either is null,
+	 * then a NullPointerException will result.
+	 *
+	 * Returns the root of the new tree, which is always the node which (used to
+	 * be) the left child of the old root.
+	 *
+	 * This method works by changing REFERENCES NOT VALUES. That is, it does not
+	 * inspect (or change) the .val field of any node; instead, it changes the
+	 * left and right pointers.
+	 *
+	 * This method updates the height of all appropriate nodes after rearranging
+	 * the references.
+	 *
+	 * This method is NON-RECURSIVE. It runs in O(1) time. As such, it may only
+	 * check the fields in a fixed number of nodes.
+	 */
+	public static AVLNode rotateRight(AVLNode subtree) {
+		throw new RuntimeException("TODO: implement me");
+	}
+
+	/*
+	 * static rotateLeft(AVLNode)
+	 *
+	 * See the documentation for rotateRight() above. This is simply its mirror
+	 * image.
+	 */
+	public static AVLNode rotateLeft(AVLNode subtree) {
+		throw new RuntimeException("TODO: implement me");
+	}
+}
