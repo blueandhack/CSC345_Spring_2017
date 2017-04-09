6302d2324f4e7a9b9c3c17268aad56dea645ba86
Yoga Lin <blueandhack@gmail.com>
2017-04-09 02:34:37 -0700

    Added somethings

--- Project04/src/BST.java
+++ Project04/src/BST.java
@@ -1,4 +1,13 @@
 public class BST implements Proj04Dictionary {
+
+	private TreeNode root;
+	private int count;
+
+	public BST() {
+		count = 0;
+		root = null;
+	}
+
 	public void insert(int key, String data) throws IllegalArgumentException {
 		throw new RuntimeException("TODO");
 	}
@@ -8,7 +17,20 @@ public class BST implements Proj04Dictionary {
 	}
 
 	public String search(int key) {
-		throw new RuntimeException("TODO");
+		return search(key, root);
+	}
+
+	private String search(int key, TreeNode node) {
+		if (node == null) {
+			return null;
+		} else if (node.key == key) {
+			return node.data;
+		} else if (node.key < key) {
+			return search(key, node.rightChild);
+		} else {
+			return search(key, node.leftChild);
+		}
+
 	}
 
 	public Integer[] getKeys() {
@@ -18,4 +40,40 @@ public class BST implements Proj04Dictionary {
 	public int getSuccessor(int key) throws IllegalArgumentException {
 		throw new RuntimeException("TODO");
 	}
+
+	// The class is tree node
+	private class TreeNode {
+		private int key;
+		private String data;
+
+		private TreeNode leftChild;
+		private TreeNode rightChild;
+
+		// Constructor, create a node but it doesn't have left and right
+		public TreeNode(int key) {
+			this.key = key;
+			this.leftChild = null;
+			this.rightChild = null;
+		}
+
+		// To get key
+		public int getKey() {
+			return key;
+		}
+
+		// Set key
+		public void setKey(int key) {
+			this.key = key;
+		}
+
+		// Check the node is it a leaf by no left and right child
+		public boolean isLeaf() {
+			if (this.leftChild == null && this.rightChild == null) {
+				return true;
+			}
+			return false;
+		}
+
+	}
+
 }
--- Project04/src/UnsortedArray.java
+++ Project04/src/UnsortedArray.java
@@ -1,21 +1,75 @@
 public class UnsortedArray implements Proj04Dictionary {
+
+	private Element first;
+	private int size;
+
+	public UnsortedArray() {
+		first = null;
+		size = 0;
+	}
+
 	public void insert(int key, String data) throws IllegalArgumentException {
-		throw new RuntimeException("TODO");
+		if (data == null) {
+			throw new IllegalArgumentException();
+		}
+		if (search(key) != null) {
+			throw new IllegalArgumentException();
+		}
+		Element ref = first;
+		if (ref == null) {
+			ref = new Element(key, data);
+			size++;
+		} else {
+			while (ref.next != null) {
+				ref = ref.next;
+			}
+			ref.next = new Element(key, data);
+			size++;
+		}
 	}
 
 	public void delete(int key) throws IllegalArgumentException {
-		throw new RuntimeException("TODO");
+		if (search(key) == null) {
+			throw new IllegalArgumentException();
+		}
+
+		Element ref = first;
+
 	}
 
 	public String search(int key) {
-		throw new RuntimeException("TODO");
+		Element ref = first;
+		if (first == null) {
+			return null;
+		}
+		while (ref.next != null) {
+			if (ref.key == key) {
+				return ref.data;
+			}
+		}
+		return null;
 	}
 
 	public Integer[] getKeys() {
-		throw new RuntimeException("TODO");
+		Integer[] array = new Integer[size];
+		int i = 0;
+		return array;
 	}
 
 	public int getSuccessor(int key) throws IllegalArgumentException {
 		throw new RuntimeException("TODO");
 	}
+
+	private class Element {
+		int key;
+		String data;
+		Element next;
+
+		public Element(int key, String data) {
+			key = key;
+			data = data;
+			next = null;
+		}
+	}
+
 }
