4136edb35050d08b4be4a8f8a527c3def7585ecd
Yoga Lin <blueandhack@gmail.com>
2017-04-11 17:23:03 -0700

    Updated Project 04

--- Project04/src/AVLTree.java
+++ Project04/src/AVLTree.java
@@ -1,14 +1,38 @@
 public class AVLTree implements Proj04Dictionary {
+
+	private AVLNode root;
+
+	public AVLTree() {
+		root = null;
+	}
+
 	public void insert(int key, String data) throws IllegalArgumentException {
-		throw new RuntimeException("TODO");
+		if (search(key) != null) {
+			throw new IllegalArgumentException();
+		}
+
 	}
 
 	public void delete(int key) throws IllegalArgumentException {
-		throw new RuntimeException("TODO");
+		if (search(key) == null) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	public String search(int key) {
-		throw new RuntimeException("TODO");
+		return search(root, key);
+	}
+
+	private String search(AVLNode node, int key) {
+		if (node == null) {
+			return null;
+		} else if (node.key == key) {
+			return node.data;
+		} else if (node.key < key) {
+			return search(node.right, key);
+		} else {
+			return search(node.left, key);
+		}
 	}
 
 	public Integer[] getKeys() {
@@ -17,5 +41,21 @@ public class AVLTree implements Proj04Dictionary {
 
 	public int getSuccessor(int key) throws IllegalArgumentException {
 		throw new RuntimeException("TODO");
+	}
+
+	private class AVLNode {
+		private AVLNode left, right;
+		private int height;
+
+		private int key;
+		private String data;
+
+		public AVLNode(int key, String data) {
+			this.key = key;
+			this.data = data;
+
+			left = right = null;
+			height = 0;
+		}
 	}
 }
