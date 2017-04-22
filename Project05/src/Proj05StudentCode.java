7a7c6b28d5895ab50d0980654ae0ff08d5b5450e
Yoga Lin <blueandhack@gmail.com>
2017-04-21 19:40:56 -0700

    Added Project 05

---
+++ Project05/src/Proj05StudentCode.java
@@ -0,0 +1,56 @@
+/* class Proj05StudentCode
+ *
+ * This is the file that the students must turn in.  The instructors have
+ * provided the skeleton for it.
+ *
+ * Skeleton code: Russell Lewis
+ * Author:        TODO
+ */
+
+
+import java.io.*;
+
+
+public class Proj05StudentCode
+{
+	/* this must open up the filename specified, and then print out a
+	 * complete .dot file, which includes all of the information from
+	 * the graph.
+	 */
+	public static void printDotFile(Proj05Vertex[] verts, String filename)
+		throws IOException
+	{
+throw new RuntimeException("TODO");
+	}
+
+
+	/* this checks to see if one index is reachable from another.  It
+	 * prints out the solution to System.out.
+	 *
+	 * The 'fromIndx' and 'toIndx' are the indices, in the verts[]
+	 * array parameter, of the endpoints of the search.  The path must
+	 * start at the 'from' node, and get to the 'to' node.
+	 */
+	public static void reachable(Proj05Vertex[] verts,
+	                             int fromIndx, int toIndx)
+	{
+throw new RuntimeException("TODO");
+	}
+
+
+	/* this must run dijkstra's algorithm.  It prints out the solution
+	 * to System.out - which will include both the path, and also the
+	 * total length of the path.
+	 *
+	 * The 'fromIndx' and 'toIndx' are the indices, in the verts[]
+	 * array parameter, of the endpoints of the search.  The path must
+	 * start at the 'from' node, and get to the 'to' node.  And, of
+	 * course, it must also be optimal.
+	 */
+	public static void dijkstra(Proj05Vertex[] verts,
+	                            int fromIndx, int toIndx)
+	{
+throw new RuntimeException("TODO");
+	}
+}
+
